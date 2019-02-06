351193b9eee57e547f93ee8886837a1d2d467d22
Sarabveer Singh <me@sarabveer.me>
2019-02-06 17:51:24 -0500

    Implement v2 Response Layout

--- src/v2/index.js
+++ src/v2/index.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 
 import getSources from './routes/sources'
+import getShabad from './routes/shabad'
 import getPage from './routes/page'
 
 const api = Router()
@@ -16,22 +17,29 @@ api.get( '/', ( req, res ) => (
 // Get List of Sources
 api.get( '/sources', ( _, res ) => (
   getSources()
-    .then( sources => res.json( sources ) )
-    .catch( err => res.status( 400 ).json( err ) )
+    .then( result => res.json( { ...result, error: false } ) )
+    .catch( err => res.status( 400 ).json( { error: true, ...err } ) )
+) )
+
+// Get Shabad from id
+api.get( '/shabad/:shabadId', ( { params: { shabadId } }, res ) => (
+  getShabad( shabadId )
+    .then( result => res.json( { ...result, error: false } ) )
+    .catch( err => res.status( 400 ).json( { error: true, ...err } ) )
 ) )
 
 // Get Ang from Sri Guru Granth Sahib Ji
-api.get( '/ang/:pageId', ( { params: { pageId } }, res ) => (
-  getPage( 1, pageId )
-    .then( lines => res.json( lines ) )
-    .catch( err => res.status( 400 ).json( err ) )
+api.get( '/ang/:pageNum', ( { params: { pageNum } }, res ) => (
+  getPage( 1, pageNum )
+    .then( result => res.json( { ...result, error: false } ) )
+    .catch( err => res.status( 400 ).json( { pageno: pageNum, error: true, ...err } ) )
 ) )
 
 // Get Page of specific source
-api.get( '/ang/:pageId/:sourceId', ( { params: { sourceId, pageId } }, res ) => (
-  getPage( sourceId, pageId )
-    .then( lines => res.json( lines ) )
-    .catch( err => res.status( 400 ).json( err ) )
+api.get( '/ang/:pageNum/:sourceId', ( { params: { pageNum, sourceId } }, res ) => (
+  getPage( sourceId, pageNum )
+    .then( result => res.json( { ...result, error: false } ) )
+    .catch( err => res.status( 400 ).json( { pageno: pageNum, error: true, ...err } ) )
 ) )
 
 export default api
