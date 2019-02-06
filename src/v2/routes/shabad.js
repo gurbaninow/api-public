0cb3a293fa8c08aac46afc2a1cc776f49c847e5e
Sarabveer Singh <me@sarabveer.me>
2019-02-06 18:21:29 -0500

    Implement v2 response layout on shabad route

--- src/v2/routes/shabad.js
+++ src/v2/routes/shabad.js
@@ -13,5 +13,107 @@ const getShabad = shabadId => Shabads.query()
   .withTranslations( translationSources )
   .withTransliterations( [ 1, 4 ] )
   .then( ( [ shabad ] ) => shabad )
+  .then( shabad => {
+    const shabadLines = {
+      shabadinfo: {
+        shabadid: shabad.id,
+        pageno: shabad.lines[ 0 ].sourcePage,
+        source: {
+          id: shabad.source.id,
+          akhar: shabad.source.nameGurmukhi,
+          unicode: toUnicode( shabad.source.nameGurmukhi ),
+          english: shabad.source.nameEnglish,
+          length: shabad.source.length,
+          pageName: {
+            akhar: shabad.source.pageNameGurmukhi,
+            unicode: toUnicode( shabad.source.pageNameGurmukhi ),
+            english: shabad.source.pageNameEnglish,
+          },
+        },
+        writer: {
+          id: shabad.writer.id,
+          akhar: shabad.writer.nameGurmukhi,
+          unicode: toUnicode( shabad.writer.nameGurmukhi ),
+          english: shabad.writer.nameEnglish,
+        },
+        raag: {
+          id: shabad.section.id,
+          akhar: shabad.section.nameGurmukhi,
+          unicode: toUnicode( shabad.section.nameGurmukhi ),
+          english: shabad.section.nameEnglish,
+          startang: shabad.section.startPage,
+          endang: shabad.section.endPage,
+          raagwithpage: `${shabad.section.nameEnglish} (${shabad.section.startPage}-${shabad.section.endPage})`,
+        },
+        count: shabad.lines.length,
+      },
+      shabad: [],
+    }
+    shabad.lines.forEach( ( line, index ) => {
+      shabadLines.shabad.push( {
+        line: {
+          id: line.id,
+          gurmukhi: {
+            akhar: line.gurmukhi.replace( /[;,.]/ug, '' ),
+            unicode: toUnicode( line.gurmukhi ).replace( /[;,.]/ug, '' ),
+          },
+          larivaar: {
+            akhar: line.gurmukhi
+              .replace( /\s/ug, '\u200B' ),
+            unicode: toUnicode( line.gurmukhi )
+              .replace( /\s/ug, '\u200B' ),
+          },
+          translation: {
+            english: {
+              default: '',
+            },
+            punjabi: {
+              default: {
+                akhar: '',
+                unicode: '',
+              },
+            },
+            spanish: '',
+          },
+          transliteration: {
+            english: {
+              text: line.transliterations[ 0 ].transliteration
+                .replace( /[;,.]/ug, '' ),
+              larivaar: line.transliterations[ 0 ].transliteration
+                .replace( /[;,.]/ug, '' )
+                .replace( /\s/ug, '\u200B' ),
+            },
+            devanagari: {
+              text: line.transliterations[ 1 ].transliteration
+                .replace( /[;,.]/ug, '' ),
+              larivaar: line.transliterations[ 1 ].transliteration
+                .replace( /[;,.]/ug, '' )
+                .replace( /\s/ug, '\u200B' ),
+            },
+          },
+          linenum: line.sourceLine,
+          firstletters: {
+            akhar: line.firstLetters,
+            unicode: toUnicode( line.firstLetters ),
+          },
+        },
+      } )
+      line.translations.forEach( translation => {
+        if ( translation.translationSource.language.id === 1 ) {
+          shabadLines.shabad[ index ].line.translation.english.default = translation.translation
+        }
+        if ( translation.translationSource.language.id === 2 ) {
+          shabadLines.shabad[ index ].line.translation.punjabi.default = {
+            akhar: toAscii( translation.translation ),
+            unicode: translation.translation,
+          }
+        }
+        if ( translation.translationSource.language.id === 3 ) {
+          shabadLines.shabad[ index ].line.translation.spanish = translation.translation
+        }
+      } )
+    } )
+    return shabadLines
+  } )
 
 export default getShabad
