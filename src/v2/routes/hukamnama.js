5d94246d63e600d8474f3022ec0d75b7ca3b9123
Sarabveer Singh <me@sarabveer.me>
2019-02-08 17:44:50 -0500

    Cleanup to use async/await for promises

--- src/v2/routes/hukamnama.js
+++ src/v2/routes/hukamnama.js
@@ -15,8 +15,9 @@ import translationSources from '../translationSources'
  * A date can be provided access Hukamnama Archives,
  * otherwise today's Hukamnama is returned.
  * @param {object} [date] JavaScript Date object
+ * @async
  */
-const getHukamnama = ( date = false ) => {
+const getHukamnama = async ( date = false ) => {
   const hukamUrl = new URL( 'http://www.sikhnet.com/webapps/gmcws/hukam.php' )
 
   if ( date ) {
@@ -25,178 +26,166 @@ const getHukamnama = ( date = false ) => {
     } )
   }
 
-  return fetch( hukamUrl )
-    .then( res => res.text() )
-    .then( body => parser.parse( body ) )
-    .then( ( { results } ) => results )
-    // eslint-disable-next-line camelcase
-    .then( ( { hukam_date, sids } ) => {
-      let hukamDate
-      try {
-        hukamDate = new Date( Date.parse( hukam_date ) )
-      } catch ( e ) {
-        throw new Error( `Error with SikhNet Source. Internal error: ${e.message}` )
-      }
-      if ( date && date.getFullYear() < 2002 ) {
-        throw new Error( 'Hukamnama Archives not available before 2002.' )
-      } else if ( date && date.getDate() !== hukamDate.getDate() ) {
-        throw new Error( 'Hukamnama not available for this day.' )
-      }
+  // Fetch Hukam Data
+  const hukamData = await fetch( hukamUrl )
+  const reqBody = await hukamData.text()
+  // eslint-disable-next-line camelcase
+  const { results: { hukam_date, sids } } = parser.parse( reqBody )
 
-      const hukamPromise = sids
-        .toString()
-        .split( ',' )
-        .reduce( ( query, shabadId ) => query.orWhere( 'shabads.sttm_id', +shabadId ), Shabads.query() )
-        .eager( '[writer, section, source, lines]' )
-        .withTranslations( translationSources )
-        .withTransliterations( [ 1, 4 ] )
-        .then( shabads => ( { date: hukamDate, shabads } ) )
+  let hukamDate
+  try {
+    hukamDate = new Date( Date.parse( hukam_date ) )
+  } catch ( e ) {
+    throw new Error( `Error with SikhNet Source. Internal error: ${e.message}` )
+  }
+  if ( date && date.getFullYear() < 2002 ) {
+    throw new Error( 'Hukamnama Archives not available before 2002.' )
+  } else if ( date && date.getDate() !== hukamDate.getDate() ) {
+    throw new Error( 'Hukamnama not available for this day.' )
+  }
 
-      return hukamPromise
-    } )
-    .then( hukam => {
-      let nanakshahiDate
-      try {
-        nanakshahiDate = getNanakshahiDate( hukam.date )
-      } catch ( e ) {
-        // Use Bikrami Calendar for date before 1 Chet, 535 NS (Nanakshahi Adoption)
-        const { solarDate } = getPanchang( date )
-        solarDate.englishDate.year -= 1525
-        solarDate.punjabiDate.year = toUnicode( solarDate.englishDate.year.toString() )
-        nanakshahiDate = { ...solarDate }
-      }
+  const shabadIds = sids.toString().split( ',' )
 
-      const [ firstShabad ] = hukam.shabads
-      const shabadIds = hukam.shabads.map( ( { id } ) => id )
-      const count = hukam.shabads.reduce( ( acc, { lines } ) => acc + lines.length, 0 )
+  const hukam = await shabadIds
+    .reduce( ( query, shabadId ) => query.orWhere( 'shabads.sttm_id', +shabadId ), Shabads.query() )
+    .eager( '[writer, section, source, lines]' )
+    .withTranslations( translationSources )
+    .withTransliterations( [ 1, 4 ] )
+    .then( shabads => ( { date: hukamDate, shabads } ) )
 
-      const hukamLines = {
-        date: {
-          gregorian: {
-            month: months[ hukam.date.getMonth() ],
-            monthno: hukam.date.getMonth() + 1,
-            date: hukam.date.getDate(),
-            year: hukam.date.getFullYear(),
-            day: days[ hukam.date.getDay() ],
-          },
-          nanakshahi: {
-            english: {
-              month: nanakshahiDate.englishDate.monthName,
-              monthno: nanakshahiDate.englishDate.month,
-              date: nanakshahiDate.englishDate.date,
-              year: nanakshahiDate.englishDate.year,
-              day: nanakshahiDate.englishDate.day,
-            },
-            punjabi: {
-              month: nanakshahiDate.punjabiDate.monthName,
-              monthno: nanakshahiDate.punjabiDate.month,
-              date: nanakshahiDate.punjabiDate.date,
-              year: nanakshahiDate.punjabiDate.year,
-              day: nanakshahiDate.punjabiDate.day,
-            },
+  let nanakshahiDate
+  try {
+    nanakshahiDate = getNanakshahiDate( hukam.date )
+  } catch ( e ) {
+    // Use Bikrami Calendar for date before 1 Chet, 535 NS (Nanakshahi Adoption)
+    const { solarDate } = getPanchang( date )
+    solarDate.englishDate.year -= 1525
+    solarDate.punjabiDate.year = toUnicode( solarDate.englishDate.year.toString() )
+    nanakshahiDate = { ...solarDate }
+  }
+
+  const [ firstShabad ] = hukam.shabads
+  const count = hukam.shabads.reduce( ( acc, { lines } ) => acc + lines.length, 0 )
+
+  const hukamLines = {
+    date: {
+      gregorian: {
+        month: months[ hukam.date.getMonth() ],
+        monthno: hukam.date.getMonth() + 1,
+        date: hukam.date.getDate(),
+        year: hukam.date.getFullYear(),
+        day: days[ hukam.date.getDay() ],
+      },
+      nanakshahi: {
+        english: {
+          month: nanakshahiDate.englishDate.monthName,
+          monthno: nanakshahiDate.englishDate.month,
+          date: nanakshahiDate.englishDate.date,
+          year: nanakshahiDate.englishDate.year,
+          day: nanakshahiDate.englishDate.day,
+        },
+        punjabi: {
+          month: nanakshahiDate.punjabiDate.monthName,
+          monthno: nanakshahiDate.punjabiDate.month,
+          date: nanakshahiDate.punjabiDate.date,
+          year: nanakshahiDate.punjabiDate.year,
+          day: nanakshahiDate.punjabiDate.day,
+        },
+      },
+    },
+    hukamnamainfo: {
+      shabadid: hukam.shabads.map( ( { id } ) => id ),
+      pageno: firstShabad.lines[ 0 ].sourcePage,
+      source: {
+        id: firstShabad.source.id,
+        akhar: firstShabad.source.nameGurmukhi,
+        unicode: toUnicode( firstShabad.source.nameGurmukhi ),
+        english: firstShabad.source.nameEnglish,
+        length: firstShabad.source.length,
+        pageName: {
+          akhar: firstShabad.source.pageNameGurmukhi,
+          unicode: toUnicode( firstShabad.source.pageNameGurmukhi ),
+          english: firstShabad.source.pageNameEnglish,
+        },
+      },
+      writer: {
+        id: firstShabad.writer.id,
+        akhar: firstShabad.writer.nameGurmukhi,
+        unicode: toUnicode( firstShabad.writer.nameGurmukhi ),
+        english: firstShabad.writer.nameEnglish,
+      },
+      raag: {
+        id: firstShabad.section.id,
+        akhar: firstShabad.section.nameGurmukhi,
+        unicode: toUnicode( firstShabad.section.nameGurmukhi ),
+        english: firstShabad.section.nameEnglish,
+        startang: firstShabad.section.startPage,
+        endang: firstShabad.section.endPage,
+        raagwithpage: `${firstShabad.section.nameEnglish} (${firstShabad.section.startPage}-${firstShabad.section.endPage})`,
+      },
+      count,
+    },
+    hukamnama: [],
+  }
+
+  hukamLines.hukamnama = hukam.shabads.reduce( ( lines, shabad ) => ( [
+    ...lines,
+    ...shabad.lines.map( ( {
+      id,
+      gurmukhi,
+      translations,
+      transliterations,
+      sourceLine: linenum,
+      firstLetters,
+    } ) => ( { line: {
+      id,
+      gurmukhi: {
+        akhar: stripVishraams( gurmukhi ),
+        unicode: toUnicode( stripVishraams( gurmukhi ) ),
+      },
+      larivaar: {
+        akhar: textLarivaar( stripVishraams( gurmukhi ) ),
+        unicode: textLarivaar( toUnicode( stripVishraams( gurmukhi ) ) ),
+      },
+      translation: {
+        english: {
+          default: translations.find( (
+            ( { translationSource: { language: { id } } } ) => id === 1 ) ).translation,
+        },
+        punjabi: {
+          default: {
+            akhar: toAscii( translations.find( (
+              ( { translationSource: { language: { id } } } ) => id === 2 ) ).translation ),
+            unicode: translations.find( (
+              ( { translationSource: { language: { id } } } ) => id === 2 ) ).translation,
           },
         },
-        hukamnamainfo: {
-          shabadid: shabadIds,
-          pageno: firstShabad.lines[ 0 ].sourcePage,
-          source: {
-            id: firstShabad.source.id,
-            akhar: firstShabad.source.nameGurmukhi,
-            unicode: toUnicode( firstShabad.source.nameGurmukhi ),
-            english: firstShabad.source.nameEnglish,
-            length: firstShabad.source.length,
-            pageName: {
-              akhar: firstShabad.source.pageNameGurmukhi,
-              unicode: toUnicode( firstShabad.source.pageNameGurmukhi ),
-              english: firstShabad.source.pageNameEnglish,
-            },
-          },
-          writer: {
-            id: firstShabad.writer.id,
-            akhar: firstShabad.writer.nameGurmukhi,
-            unicode: toUnicode( firstShabad.writer.nameGurmukhi ),
-            english: firstShabad.writer.nameEnglish,
-          },
-          raag: {
-            id: firstShabad.section.id,
-            akhar: firstShabad.section.nameGurmukhi,
-            unicode: toUnicode( firstShabad.section.nameGurmukhi ),
-            english: firstShabad.section.nameEnglish,
-            startang: firstShabad.section.startPage,
-            endang: firstShabad.section.endPage,
-            raagwithpage: `${firstShabad.section.nameEnglish} (${firstShabad.section.startPage}-${firstShabad.section.endPage})`,
-          },
-          count,
+        spanish: translations.find( (
+          ( { translationSource: { language: { id } } } ) => id === 3 ) ).translation,
+      },
+      transliteration: {
+        english: {
+          text: stripVishraams( transliterations.find( (
+            ( { language: { id } } ) => id === 1 ) ).transliteration ),
+          larivaar: textLarivaar( stripVishraams( transliterations.find( (
+            ( { language: { id } } ) => id === 1 ) ).transliteration ) ),
         },
-        hukamnama: [],
-      }
+        devanagari: {
+          text: stripVishraams( transliterations.find( (
+            ( { language: { id } } ) => id === 4 ) ).transliteration ),
+          larivaar: textLarivaar( stripVishraams( transliterations.find( (
+            ( { language: { id } } ) => id === 4 ) ).transliteration ) ),
+        },
+      },
+      linenum,
+      firstletters: {
+        akhar: firstLetters,
+        unicode: toUnicode( firstLetters ),
+      },
+    } } ) ),
+  ] ), [] )
 
-      hukam.shabads.forEach( shabad => {
-        shabad.lines.forEach( line => {
-          let english = ''
-          let punjabi = { akhar: '', unicode: '' }
-          let spanish = ''
-          line.translations.forEach( translation => {
-            if ( translation.translationSource.language.id === 1 ) {
-              english = translation.translation
-            }
-            if ( translation.translationSource.language.id === 2 ) {
-              punjabi = {
-                akhar: toAscii( translation.translation ),
-                unicode: translation.translation,
-              }
-            }
-            if ( translation.translationSource.language.id === 3 ) {
-              spanish = translation.translation
-            }
-          } )
-
-          hukamLines.hukamnama.push( {
-            line: {
-              id: line.id,
-              gurmukhi: {
-                akhar: stripVishraams( line.gurmukhi ),
-                unicode: toUnicode( stripVishraams( line.gurmukhi ) ),
-              },
-              larivaar: {
-                akhar: textLarivaar( stripVishraams( line.gurmukhi ) ),
-                unicode: textLarivaar( toUnicode( stripVishraams( line.gurmukhi ) ) ),
-              },
-              translation: {
-                english: {
-                  default: english,
-                },
-                punjabi: {
-                  default: punjabi,
-                },
-                spanish,
-              },
-              transliteration: {
-                english: {
-                  text: stripVishraams( line.transliterations[ 0 ].transliteration ),
-                  larivaar: textLarivaar(
-                    stripVishraams( line.transliterations[ 0 ].transliteration ),
-                  ),
-                },
-                devanagari: {
-                  text: stripVishraams( line.transliterations[ 1 ].transliteration ),
-                  larivaar: textLarivaar(
-                    stripVishraams( line.transliterations[ 0 ].transliteration ),
-                  ),
-                },
-              },
-              linenum: line.sourceLine,
-              firstletters: {
-                akhar: line.firstLetters,
-                unicode: toUnicode( line.firstLetters ),
-              },
-            },
-          } )
-        } )
-      } )
-
-      return hukamLines
-    } )
+  return hukamLines
 }
 
 export default getHukamnama
--- src/v2/routes/shabad.js
+++ src/v2/routes/shabad.js
@@ -7,111 +7,105 @@ import translationSources from '../translationSources'
 /**
  * Get Shabad from Shabad id
  * @param {string} shabadId The ID of the Shabad to query.
+ * @async
  */
-const getShabad = shabadId => Shabads.query()
-  .where( 'shabads.id', shabadId )
-  .eager( '[writer, section, source, lines]' )
-  .withTranslations( translationSources )
-  .withTransliterations( [ 1, 4 ] )
-  .then( ( [ shabad ] ) => shabad )
-  .then( shabad => {
-    const shabadLines = {
-      shabadinfo: {
-        shabadid: shabad.id,
-        pageno: shabad.lines[ 0 ].sourcePage,
-        source: {
-          id: shabad.source.id,
-          akhar: shabad.source.nameGurmukhi,
-          unicode: toUnicode( shabad.source.nameGurmukhi ),
-          english: shabad.source.nameEnglish,
-          length: shabad.source.length,
-          pageName: {
-            akhar: shabad.source.pageNameGurmukhi,
-            unicode: toUnicode( shabad.source.pageNameGurmukhi ),
-            english: shabad.source.pageNameEnglish,
-          },
-        },
-        writer: {
-          id: shabad.writer.id,
-          akhar: shabad.writer.nameGurmukhi,
-          unicode: toUnicode( shabad.writer.nameGurmukhi ),
-          english: shabad.writer.nameEnglish,
-        },
-        raag: {
-          id: shabad.section.id,
-          akhar: shabad.section.nameGurmukhi,
-          unicode: toUnicode( shabad.section.nameGurmukhi ),
-          english: shabad.section.nameEnglish,
-          startang: shabad.section.startPage,
-          endang: shabad.section.endPage,
-          raagwithpage: `${shabad.section.nameEnglish} (${shabad.section.startPage}-${shabad.section.endPage})`,
-        },
-        count: shabad.lines.length,
-      },
-      shabad: [],
-    }
-    shabad.lines.forEach( line => {
-      let english = ''
-      let punjabi = { akhar: '', unicode: '' }
-      let spanish = ''
-      line.translations.forEach( translation => {
-        if ( translation.translationSource.language.id === 1 ) {
-          english = translation.translation
-        }
-        if ( translation.translationSource.language.id === 2 ) {
-          punjabi = {
-            akhar: toAscii( translation.translation ),
-            unicode: translation.translation,
-          }
-        }
-        if ( translation.translationSource.language.id === 3 ) {
-          spanish = translation.translation
-        }
-      } )
+const getShabad = async shabadId => {
+  const shabadData = await Shabads.query()
+    .where( 'shabads.id', shabadId )
+    .eager( '[writer, section, source, lines]' )
+    .withTranslations( translationSources )
+    .withTransliterations( [ 1, 4 ] )
+    .then( ( [ shabad ] ) => shabad )
 
-      shabadLines.shabad.push( {
-        line: {
-          id: line.id,
-          gurmukhi: {
-            akhar: stripVishraams( line.gurmukhi ),
-            unicode: toUnicode( stripVishraams( line.gurmukhi ) ),
+  const shabadLines = {
+    shabadinfo: {
+      shabadid: shabadData.id,
+      pageno: shabadData.lines[ 0 ].sourcePage,
+      source: {
+        id: shabadData.source.id,
+        akhar: shabadData.source.nameGurmukhi,
+        unicode: toUnicode( shabadData.source.nameGurmukhi ),
+        english: shabadData.source.nameEnglish,
+        length: shabadData.source.length,
+        pageName: {
+          akhar: shabadData.source.pageNameGurmukhi,
+          unicode: toUnicode( shabadData.source.pageNameGurmukhi ),
+          english: shabadData.source.pageNameEnglish,
+        },
+      },
+      writer: {
+        id: shabadData.writer.id,
+        akhar: shabadData.writer.nameGurmukhi,
+        unicode: toUnicode( shabadData.writer.nameGurmukhi ),
+        english: shabadData.writer.nameEnglish,
+      },
+      raag: {
+        id: shabadData.section.id,
+        akhar: shabadData.section.nameGurmukhi,
+        unicode: toUnicode( shabadData.section.nameGurmukhi ),
+        english: shabadData.section.nameEnglish,
+        startang: shabadData.section.startPage,
+        endang: shabadData.section.endPage,
+        raagwithpage: `${shabadData.section.nameEnglish} (${shabadData.section.startPage}-${shabadData.section.endPage})`,
+      },
+      count: shabadData.lines.length,
+    },
+    shabad: [],
+  }
+
+  shabadLines.shabad = shabadData.lines.reduce( ( lines, line ) => ( [
+    ...lines,
+    {
+      line: {
+        id: line.id,
+        gurmukhi: {
+          akhar: stripVishraams( line.gurmukhi ),
+          unicode: toUnicode( stripVishraams( line.gurmukhi ) ),
+        },
+        larivaar: {
+          akhar: textLarivaar( stripVishraams( line.gurmukhi ) ),
+          unicode: textLarivaar( toUnicode( stripVishraams( line.gurmukhi ) ) ),
+        },
+        translation: {
+          english: {
+            default: line.translations.find( (
+              ( { translationSource: { language: { id } } } ) => id === 1 ) ).translation,
           },
-          larivaar: {
-            akhar: textLarivaar( stripVishraams( line.gurmukhi ) ),
-            unicode: textLarivaar( toUnicode( stripVishraams( line.gurmukhi ) ) ),
-          },
-          translation: {
-            english: {
-              default: english,
-            },
-            punjabi: {
-              default: punjabi,
-            },
-            spanish,
-          },
-          transliteration: {
-            english: {
-              text: stripVishraams( line.transliterations[ 0 ].transliteration ),
-              larivaar: textLarivaar(
-                stripVishraams( line.transliterations[ 0 ].transliteration ),
-              ),
-            },
-            devanagari: {
-              text: stripVishraams( line.transliterations[ 1 ].transliteration ),
-              larivaar: textLarivaar(
-                stripVishraams( line.transliterations[ 0 ].transliteration ),
-              ),
+          punjabi: {
+            default: {
+              akhar: toAscii( line.translations.find( (
+                ( { translationSource: { language: { id } } } ) => id === 2 ) ).translation ),
+              unicode: line.translations.find( (
+                ( { translationSource: { language: { id } } } ) => id === 2 ) ).translation,
             },
           },
-          linenum: line.sourceLine,
-          firstletters: {
-            akhar: line.firstLetters,
-            unicode: toUnicode( line.firstLetters ),
+          spanish: line.translations.find( (
+            ( { translationSource: { language: { id } } } ) => id === 3 ) ).translation,
+        },
+        transliteration: {
+          english: {
+            text: stripVishraams( line.transliterations.find( (
+              ( { language: { id } } ) => id === 1 ) ).transliteration ),
+            larivaar: textLarivaar( stripVishraams( line.transliterations.find( (
+              ( { language: { id } } ) => id === 1 ) ).transliteration ) ),
+          },
+          devanagari: {
+            text: stripVishraams( line.transliterations.find( (
+              ( { language: { id } } ) => id === 4 ) ).transliteration ),
+            larivaar: textLarivaar( stripVishraams( line.transliterations.find( (
+              ( { language: { id } } ) => id === 4 ) ).transliteration ) ),
           },
         },
-      } )
-    } )
-    return shabadLines
-  } )
+        linenum: line.sourceLine,
+        firstletters: {
+          akhar: line.firstLetters,
+          unicode: toUnicode( line.firstLetters ),
+        },
+      },
+    },
+  ] ), [] )
+
+  return shabadLines
+}
 
 export default getShabad
