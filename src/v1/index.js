4f2a8568bdde30cc0220fed9d3c8bb2e323325a7
Sarabveer Singh <me@sarabveer.me>
2019-07-17 11:59:41 -0400

    feat(v1): Add search endpoint

--- src/v1/index.js
+++ src/v1/index.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 
+import search from './routes/search'
 import getShabad from './routes/shabad'
 import getPage from './routes/page'
 
@@ -12,7 +13,17 @@ api.get( '/', ( req, res, next ) => {
     throw new Error( 'APIv1 mode is not given.' )
   }
   let response
-  if ( +mode === 2 ) {
+  if ( +mode === 1 ) {
+    response = search(
+      req.query.q,
+      req.query.searchtype,
+      req.query.source,
+      req.query.writer,
+      req.query.raag,
+      req.query.ang,
+      req.query.results,
+    )
+  } else if ( +mode === 2 ) {
     response = getShabad( req.query.shabadNo )
   } else if ( +mode === 3 ) {
     response = getPage( 1, +req.query.ang )
