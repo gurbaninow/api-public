9e1bef1367b1dedaecff84ea8e06c368a72cbe08
Harjot Singh <harjot@harkul.com>
2019-10-04 23:32:27 +0100

    build: add local docker-compose support

---
+++ knexfile.local.js
@@ -0,0 +1,12 @@
+const config = {
+  client: 'mysql',
+  connection: {
+    host: 'shabados-database',
+    user: 'shabados',
+    database: 'shabados',
+    port: '3306',
+  },
+  useNullAsDefault: true,
+}
+
+module.exports = config
